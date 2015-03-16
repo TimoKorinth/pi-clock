90896f5fae559e7cb151aef10f5d6913478d74e9
Timo Korinth <info@timokorinth.de>
2015-03-16 23:00:39 +0100

    Adjusted time intervals

--- piAtom.js
+++ piAtom.js
@@ -11,7 +11,7 @@
                     atom: '=?',
                     tick: '=?'
                 },
-                template: '<div class="piAtomWrapper"><div class="piAtom" ng-show="isVisible"></div></div>',
+                template: '<div class="piAtomWrapper"><div ng-class="{one:atom==1,two:atom==2,three:atom==3,four:atom==4,five:atom==5,six:atom==6,seven:atom==7,eight:atom==8,nine:atom==9}" class="piAtom" ng-show="isVisible"></div></div>',
                 link: function (scope, element, attrs) {
                     
                     scope.$watch('tick', function (newTick) {
